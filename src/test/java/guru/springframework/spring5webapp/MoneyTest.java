09dc72581097362e42857091158e715cba0243cd
ASGodolfim <stonegodolfim@gmail.com>
2023-07-21 15:26:55 -0300

    test: add test for multiplication

---
+++ src/test/java/guru/springframework/spring5webapp/MoneyTest.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webapp;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
