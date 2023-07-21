3f0352d70f6f19dacc0755ba2476b1e51c383f25
ASGodolfim <stonegodolfim@gmail.com>
2023-07-21 17:31:42 -0300

    feat: currency and testCurrency

--- src/main/java/guru/springframework/spring5webapp/currency/Dollar.java
+++ src/main/java/guru/springframework/spring5webapp/currency/Dollar.java
@@ -1,14 +1,11 @@
 package guru.springframework.spring5webapp.currency;
 
 public class Dollar extends Money {
-
-
-    public Dollar(int amount){
-        this.amount = amount;
+    public Dollar(int amount, String currency){
+        super(amount, currency);
     }
-
     public Money times (int multiplier){
-        return new Dollar (amount * multiplier);
+        return Money.dollar (amount * multiplier);
     }
 
 }
--- src/main/java/guru/springframework/spring5webapp/currency/Money.java
+++ src/main/java/guru/springframework/spring5webapp/currency/Money.java
@@ -2,17 +2,23 @@ package guru.springframework.spring5webapp.currency;
 
 public abstract class Money {
     protected int amount;
+    public String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency(){
+        return currency;
+    }
     public abstract Money times(int multiplier);
-
     public static Money dollar (int amount){
-        return new Dollar (amount);
+        return new Dollar (amount, "USD");
     }
-
     public static Money franc (int amount){
-        return new Franc (amount);
+        return new Franc(amount, "CHF");
     }
-
     public boolean equals(Object obj){
         Money money = (Money) obj;
         return amount == money.amount
