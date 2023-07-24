d97baf27b53855de50bb5a54d78e755b0c5be3c0
ASGodolfim <stonegodolfim@gmail.com>
2023-07-24 15:06:23 -0300

    test: rates

--- src/main/java/guru/springframework/spring5webapp/Money.java
+++ src/main/java/guru/springframework/spring5webapp/Money.java
@@ -35,8 +35,8 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
     @Override
-    public Money reduce(String to){
-        return this;
+    public Money reduce(Bank bank, String to){
+        return new Money(amount / bank.rate(this.currency, to), to);
     }
 
 }
