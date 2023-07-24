26b598c83c27ee4a44562a668fe14b7be570a727
ASGodolfim <stonegodolfim@gmail.com>
2023-07-24 14:40:38 -0300

    test: refator simple add and added reducesum and reducemoney

--- src/main/java/guru/springframework/spring5webapp/Bank.java
+++ src/main/java/guru/springframework/spring5webapp/Bank.java
@@ -2,6 +2,6 @@ package guru.springframework.spring5webapp;
 
 public class Bank {
     Money reduce(Expression source,String toCurrency){
-        return Money.dollar(10);
+    return source.reduce(toCurrency);
     }
 }
--- src/main/java/guru/springframework/spring5webapp/Money.java
+++ src/main/java/guru/springframework/spring5webapp/Money.java
@@ -32,9 +32,12 @@ public class Money implements Expression {
         return new Money (amount * multiplier, this.currency);
     }
     public Expression plus(Money addend){
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
-
+    @Override
+    public Money reduce(String to){
+        return this;
+    }
 
 }
 
